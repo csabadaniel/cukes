0e027c399e605bd33b94e413fdde73de2756073a
Csaba Daniel <csaba.daniel@sainsburys.co.uk>
2024-12-27 20:27:59 +0100

    Reformat code

--- src/main/java/io/github/csabadaniel/cukes/CukesApplication.java
+++ src/main/java/io/github/csabadaniel/cukes/CukesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CukesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CukesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CukesApplication.class, args);
+    }
 
 }
