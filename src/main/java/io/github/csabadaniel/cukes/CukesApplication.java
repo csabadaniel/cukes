4ea6fc9932a7d45f10c720b8730c49c8efe2c818
Csaba Daniel <csaba.daniel@sainsburys.co.uk>
2024-12-27 19:48:34 +0100

    Bootstrap Spring Initializr

---
+++ src/main/java/io/github/csabadaniel/cukes/CukesApplication.java
@@ -0,0 +1,13 @@
+package io.github.csabadaniel.cukes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CukesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CukesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/csabadaniel/cukes/CukesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.csabadaniel.cukes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CukesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
